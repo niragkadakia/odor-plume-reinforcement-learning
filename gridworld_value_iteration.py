2ad523163fab330f733297c5d198001da46ad935
nk479 <nirag.kadakia@yale.edu>
2019-10-23 09:46:05 -0400

    Add working gridworld scripts

---
+++ gridworld_value_iteration.py
@@ -0,0 +1,122 @@
+"""
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+gamma = 0.9
+
+num_s0 = 25
+num_s1 = 25
+num_actions = 4
+
+prob = np.zeros((num_s0, num_s1, num_actions))
+reward = np.zeros((num_s0, num_s1, num_actions))
+action = np.zeros((num_s0, num_actions))
+
+# Equal transition probabilities for all directions
+action[:] = 0.25
+
+# s, s', a
+col = 1
+row = 5
+u = 0
+r = 1
+d = 2
+l = 3
+
+i = 0
+prob[i, i, u] = 1
+prob[i, i + col, r] = 1
+prob[i, i + row, d] = 1
+prob[i, i, l] = 1
+
+for i in [1, 2, 3]:
+	prob[i, i, u] = 1
+	prob[i, i + col, r] = 1
+	prob[i, i + row, d] = 1 
+	prob[i, i - col, l] = 1 
+
+i = 4
+prob[i, i, u] = 1
+prob[i, i, r] = 1
+prob[i, i + row, d] = 1
+prob[i, i - col, l] = 1
+
+for i in [5, 10, 15]:
+	prob[i, i - row, u] = 1
+	prob[i, i + col, r] = 1
+	prob[i, i + row, d] = 1
+	prob[i, i, l] = 1
+
+for i in [ 6,  7,  8, 
+		  11, 12, 13, 
+		  16, 17, 18]:
+	prob[i, i - row, u] = 1
+	prob[i, i + col, r] = 1
+	prob[i, i + row, d] = 1
+	prob[i, i - col, l] = 1
+
+for i in [9, 14, 19]:
+	prob[i, i - row, u] = 1
+	prob[i, i, r] = 1
+	prob[i, i + row, d] = 1
+	prob[i, i - col, l] = 1
+
+i = 20
+prob[i, i - row, u] = 1
+prob[i, i + col, r] = 1
+prob[i, i, d] = 1
+prob[i, i, l] = 1
+
+for i in [21, 22, 23]:
+	prob[i, i - row, u] = 1
+	prob[i, i + col, r] = 1
+	prob[i, i, d] = 1
+	prob[i, i - col, l] = 1
+
+i = 24
+prob[i, i - row, u] = 1
+prob[i, i, r] = 1
+prob[i, i, d] = 1
+prob[i, i - col, l] = 1
+
+# Special states
+prob[1, :, :] = 0
+prob[3, :, :] = 0
+for a in range(4):
+	prob[1, 21, a] = 1
+	prob[3, 13, a] = 1
+
+# Rewards
+
+#reward[prob != 0] = 1
+for i in range(5):
+	reward[i, i, u] =  -1
+for i in range(0, 25, 5):
+	reward[i, i, l] =  -1
+for i in range(4, 25, 5):
+	reward[i, i, r] =  -1
+for i in range(20, 25):
+	reward[i, i, d] =  -1
+
+# Special states
+reward[1, :, :] = 0
+reward[3, :, :] = 0
+for a in range(4):
+	reward[1, 21, a] = 10.
+	reward[3, 13, a] = 5.
+
+# Get value function by iterative update
+X_old = np.zeros(num_s0)	
+for i in range(100):
+	X_new = np.zeros(num_s0)
+	for iC in range(num_s0):
+		for jC in range(num_s1):
+			for aC in range(num_actions):
+				X_new[iC] += action[iC, aC]*prob[iC, jC, aC]*\
+						(reward[iC, jC, aC] + gamma*X_old[jC])
+	X_old[:] = X_new[:]
+	
+print (X_new)
---
+++ multi_armed_bandits.py
@@ -0,0 +1,46 @@
+"""
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+num_trials = 1000
+num_bandits = 2000
+num_actions = 10
+
+# Greedy means epsilon = 0. % of time wrong sub-optimal arbitrary chosen
+eps = 0
+
+# For each bandits, this will hold the actions (E[reward]) of each action
+np.random.seed(0)
+actions = np.random.normal(0, 1, (num_actions, num_bandits))
+
+# Holds estimate of action value and number of times that action chosen
+Q = np.random.normal(0, 1, (num_actions, num_bandits))
+N = np.zeros(Q.shape)
+
+# Holds average rewards on each step and % of time optimal action taken
+avg_R = []
+avg_opt_A = []
+
+for _ in range(num_trials):
+	
+	# Action is optimum of estimated rewards, plus non-greedy randomness
+	A = np.argmax(Q, axis=0)
+	rand_flips = np.random.uniform(0, 1, len(A)) < eps
+	A[rand_flips] = np.random.randint(0, 10, np.sum(rand_flips))
+	avg_opt_A.append(np.mean(A == np.argmax(actions, axis=0)))
+	
+	N[A, range(num_bandits)] += 1
+	R = np.random.normal(actions[A, range(num_bandits)], 1)
+	
+	Q[A, range(num_bandits)] += 1./(N[A, range(num_bandits)])*\
+	  (R - Q[A, range(num_bandits)]) 
+	
+	avg_R.append(np.mean(R))
+	
+plt.plot(avg_R)
+plt.plot(avg_opt_A)
+plt.show()
+
