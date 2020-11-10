44a0f13267ab4253ad1bab1cb4e98499366c00da
nk479 <nirag.kadakia@yale.edu>
2020-11-09 21:30:30 -0500

    Add freq+dur model

--- src/models.py
+++ src/models.py
@@ -53,6 +53,10 @@ class odor_series_RL(object):
 		# epsilon-greedy action selection
 		self.epsilon = 0.05
 
+		# Condition for reaching source
+		self.x_end = 150
+		self.y_end_rad = 25
+
 	def def_states(self, odor_threshold=100, odor_vec_len=500, 
 				   freq_bins=[-0.01, 2, 100], tau=2):
 		"""
@@ -146,9 +150,14 @@ class odor_series_RL(object):
 		Update navigator positions given selected actions
 		"""
 
-		_xs = xs[iS]
-		_ys = ys[iS]
-		_vs = vs[iS]
+
+		_xs = np.empy(self.num_walkers)*np.nan
+		_ys = np.empy(self.num_walkers)*np.nan
+		_vs = np.empy(self.num_walkers)*np.nan
+		
+		_xs[:] = xs[iS]
+		_ys[:] = ys[iS]
+		_vs[:] = vs[iS]
 		
 		# Update _xs, _ys, _vs based on some algorithm
 		
@@ -259,7 +268,8 @@ class odor_series_RL(object):
 		Determine paths that reached source.
 		"""
 		
-		_ends = (xs[iS] < 150)*(abs(ys[iS] - self.y_center) < 50)
+		_ends = (xs[iS] < self.x_end)*\
+				  (abs(ys[iS] - self.y_center) < self.y_end_rad)
 		path_lengths[(ends == 0)*_ends] = iS
 		ends[_ends] = 1
 
@@ -355,8 +365,11 @@ class centerline_upwind_downwind(odor_series_RL):
 		Update navigator positions given selected actions
 		"""
 
-		_xs = xs[iS]
-		_ys = ys[iS]
+		_xs = np.empy(self.num_walkers)*np.nan
+		_ys = np.empy(self.num_walkers)*np.nan
+				
+		_xs[:] = xs[iS]
+		_ys[:] = ys[iS]
 
 		_xs[_xs > self.max_x] = self.max_x
 		_ys[_ys > self.max_y] = self.max_y
@@ -383,7 +396,7 @@ class centerline_upwind_downwind(odor_series_RL):
 		_ys[_vs == 1] += self.xy_step
 		_xs[_vs == 2] -= self.xy_step
 		_ys[_vs == 3] -= self.xy_step
-
+		
 		xs[iS + 1] = _xs
 		ys[iS + 1] = _ys
 		vs[iS + 1] = _vs
@@ -416,8 +429,11 @@ class centerline_upwind_downwind_turns(odor_series_RL):
 		Update navigator positions given selected actions
 		"""
 
-		_xs = xs[iS]
-		_ys = ys[iS]
+		_xs = np.empy(self.num_walkers)*np.nan
+		_ys = np.empy(self.num_walkers)*np.nan
+		
+		_xs[:] = xs[iS]
+		_ys[:] = ys[iS]
 
 		_xs[_xs > self.max_x] = self.max_x
 		_ys[_ys > self.max_y] = self.max_y
@@ -521,13 +537,11 @@ class stochastic_turning(odor_series_RL):
 		probability p at any given step.
 		"""
 
-		_xs = xs[iS]
-		_ys = ys[iS]
+		_xs = np.empty(self.num_walkers)*np.nan
+		_ys = np.empty(self.num_walkers)*np.nan
 
-		_xs[_xs > self.max_x] = self.max_x
-		_ys[_ys > self.max_y] = self.max_y
-		_xs[_xs < 0] = 0
-		_ys[_ys < 0] = 0
+		_xs[:] = xs[iS]
+		_ys[:] = ys[iS]
 
 		# Turns only happen with prob p at each step sample point. `Straights'
 		# are like turns but with smaller magnitude
@@ -559,11 +573,25 @@ class stochastic_turning(odor_series_RL):
 									 turns[s2*(actions == 1)]
 
 		_thetas = np.mod(_thetas, 2*np.pi)
-		_xs = (np.cos(_thetas)*self.xy_step).astype(int)
-		_ys = (np.sin(_thetas)*self.xy_step).astype(int)
+		dxs = (np.cos(_thetas)*self.xy_step).astype(int)
+		dys = (np.sin(_thetas)*self.xy_step).astype(int)
+
+		_xs += dxs
+		_ys += dys
 		
-		xs[iS + 1] = xs[iS] + _xs
-		ys[iS + 1] = ys[iS] + _ys
+		# Enforce boundaries
+		_xs[_xs <= 0] = 0
+		_xs[_xs >= self.max_x] = self.max_x
+		_ys[_ys <= 0] = 0
+		_ys[_ys >= self.max_y] = self.max_y
+		
+		# Agents at front wall at last step are stuck
+		## TODO Put this in path lengths section instead.
+		_xs[xs[iS] == 0] = 0
+		_ys[xs[iS] == 0] = ys[iS, xs[iS] == 0]
+		
+		xs[iS + 1] = _xs
+		ys[iS + 1] = _ys
 		thetas[iS + 1] = _thetas
 
 		return xs, ys, thetas
@@ -631,6 +659,251 @@ class stochastic_turning(odor_series_RL):
 			
 			# Update odor vector and state
 			whfs, whf_freqs, odors, states = \
+				self.update_states(xs, ys, states, odors, iS)
+		
+		return xs, ys, path_lengths
+
+
+class stochastic_turning_dur_freq(odor_series_RL):
+	"""
+	Reinforced learning simulation where navigators can i) turn upwind with 
+	angle ~N(30, 10) degrees, ii) turn downwind with same angle. This class
+	bins both odor duration and odor frequency to define the states.
+	"""
+
+	def __init__(self):
+		super().__init__()
+
+		self.turn_prob = 0.2
+		
+		self.turn_angle_avg = 30
+		self.turn_angle_std = 10
+		
+		# Multiplier to get angle of straight, based on angle of turn
+		self.straight_multiplier = 0.2
+
+	def def_states(self, odor_threshold=100, odor_vec_len=500, 
+				   freq_bins=[-0.01, 2, 100], dur_bins=[0, 0.2, 5],
+				   tau=2):
+		"""
+		States are delineated by odor frequency and duration, which is 
+		defined by an exponential timescale
+		"""
+
+		self.odor_threshold = odor_threshold
+		self.odor_vec_len = odor_vec_len
+		self.freq_bins = freq_bins
+		self.dur_bins = dur_bins
+		self.tau = tau
+		self._num_states = (len(self.freq_bins) - 1)*(len(self.dur_bins)  - 1)
+
+	def def_actions(self):
+		"""
+		Actions defined by motion `v' in run script
+		"""
+
+		self._num_actions = 2
+
+	def init_vars(self):
+		"""
+		Initialize data structures used in simulation
+		"""
+
+		xs = np.zeros((self.num_steps, self.num_walkers), dtype=int)
+		ys = np.zeros((self.num_steps, self.num_walkers), dtype=int)
+		thetas = np.zeros((self.num_steps, self.num_walkers))
+		states = np.zeros((self.num_steps, self.num_walkers), dtype=int)
+		Q = np.ones((self.num_steps, self._num_states, 
+					 self._num_actions, self.num_walkers))
+
+		# Binary vectory indicating if path reached source
+		ends = np.zeros(self.num_walkers)
+
+		# Length of trajectory from initial point to source in timesteps
+		path_lengths = np.ones(self.num_walkers).astype(int)*self.num_steps
+
+		# Initial x,y are random; initial motion is randomly oriented
+		xs[0] = np.random.randint(self.x0_min, 
+								  self.x0_max, 
+								  self.num_walkers)
+		ys[0] = np.random.randint(self.y_center - self.y0_spread, 
+								  self.y_center + self.y0_spread, 
+								  self.num_walkers)
+		thetas[0] = np.random.uniform(0, np.pi*2, self.num_walkers)
+
+		# Holds odor vector to define states
+		odors = np.zeros((self.odor_vec_len, self.num_walkers))
+
+		return xs, ys, thetas, states, Q, ends, path_lengths, odors
+
+	def update_xys(self, xs, ys, thetas, actions, iS):
+		"""
+		Update navigator positions given selected actions. Actions
+		are turn upwind or turn downwind. Angle is chosen from a normal
+		distribution of mean 30 and dev 10. Turns only occur with 
+		probability p at any given step.
+		"""
+
+		_xs = np.empty(self.num_walkers)*np.nan
+		_ys = np.empty(self.num_walkers)*np.nan
+
+		_xs[:] = xs[iS]
+		_ys[:] = ys[iS]
+
+		# Turns only happen with prob p at each step sample point. `Straights'
+		# are like turns but with smaller magnitude
+		turns = np.random.normal(self.turn_angle_avg*np.pi/180, 
+				self.turn_angle_std*np.pi/180, self.num_walkers)
+		straight_bin = np.random.choice([0, 1], size=self.num_walkers, 
+				   p=[self.turn_prob, 1 - self.turn_prob])
+		rand_dirs = np.random.randint(0, 2, self.num_walkers)*2 - 1
+		turns[straight_bin] *= self.straight_multiplier*rand_dirs
+
+		_thetas = np.empty(self.num_walkers)*np.nan
+		s1 = np.mod(thetas[iS], 2*np.pi) < np.pi
+		s2 = np.mod(thetas[iS], 2*np.pi) >= np.pi
+
+		# Top half and turn upwind
+		_thetas[s1*(actions == 0)] = thetas[iS, s1*(actions == 0)] + \
+									 turns[s1*(actions == 0)]
+
+		# Bottom half and turn upwind
+		_thetas[s2*(actions == 0)] = thetas[iS, s2*(actions == 0)] - \
+									 turns[s2*(actions == 0)]
+
+		# Top half and turn downwind
+		_thetas[s1*(actions == 1)] = thetas[iS, s1*(actions == 1)] - \
+									 turns[s1*(actions == 1)]
+
+		# Bottom half and turn downwind
+		_thetas[s2*(actions == 1)] = thetas[iS, s2*(actions == 1)] + \
+									 turns[s2*(actions == 1)]
+
+		_thetas = np.mod(_thetas, 2*np.pi)
+		dxs = (np.cos(_thetas)*self.xy_step).astype(int)
+		dys = (np.sin(_thetas)*self.xy_step).astype(int)
+
+		_xs += dxs
+		_ys += dys
+		
+		# Enforce boundaries
+		_xs[_xs <= 0] = 0
+		_xs[_xs >= self.max_x] = self.max_x
+		_ys[_ys <= 0] = 0
+		_ys[_ys >= self.max_y] = self.max_y
+		
+		# Agents at front wall at last step are stuck
+		## TODO Put this in path lengths section instead.
+		_xs[xs[iS] == 0] = 0
+		_ys[xs[iS] == 0] = ys[iS, xs[iS] == 0]
+		
+		xs[iS + 1] = _xs
+		ys[iS + 1] = _ys
+		thetas[iS + 1] = _thetas
+
+		return xs, ys, thetas
+
+	def update_states(self, xs, ys, states, odors, iS):
+		"""
+		Update the odor vector and the next state based on odor signal
+		"""
+
+		frm = self.update_vid_frm()
+		
+		# Whiff is binary; 1 if greater than odor threshold
+		whfs = 1.*(frm[(ys[iS], xs[iS])] > self.odor_threshold)
+
+		# Append new signal to odor vector (odors in recent past)
+		odors = np.roll(odors, -1, axis=0)
+		odors[-1] = whfs
+
+		# Freq of odor whiffs by discounting recent odor hits (diff of odors)
+		# with a 2s decaying exponential
+		whf_freqs = np.sum((np.diff(odors, axis=0) > 0).T*
+						  np.exp(-np.arange(odors.shape[0] - 1).T
+						  /self.tau/self.fps)[::-1], axis=1)
+		whf_durs = np.sum((odors[1:] > 0).T*1/self.fps*
+						  np.exp(-np.arange(odors.shape[0] - 1).T
+						  /self.tau/self.fps)[::-1], axis=1)
+		
+		# Outer loop is dur, inner loop is freq
+		outer_n = np.digitize(whf_durs, self.dur_bins, right=True) - 1
+		inner_n = np.digitize(whf_freqs, self.freq_bins, right=True) - 1
+		
+		states[iS + 1] = outer_n*(len(self.freq_bins) - 1) + inner_n
+
+		return whfs, whf_freqs, whf_durs, odors, states
+
+	def update_rewards(self, whfs, whf_freqs, whf_durs, odors):
+		"""
+		Update the rewards at this step
+		"""
+		
+		return whfs
+	
+	def run(self, seed=0):
+		"""
+		Do 1 run of LR to get Q-function
+		"""
+
+		np.random.seed(seed)
+		self.init_vid()
+		xs, ys, thetas, states, Q, ends, path_lengths, odors = self.init_vars()
+		
+		for iS in range(self.num_steps - 1):
+			sys.stdout.write('\r')
+			sys.stdout.write("{:.1f}%".format((100/(self.num_steps - 1)*iS)))
+			sys.stdout.flush()
+
+			# Update actions and positions based on optimal actions
+			actions = self.update_actions_from_Q(Q, states, iS)
+			xs, ys, thetas = self.update_xys(xs, ys, thetas, actions, iS)
+
+			# Mark those that reached the source and how long to do so
+			ends, path_lengths = \
+				self.get_terminal_paths(xs, ys, ends, path_lengths, iS)
+			
+			# Update odor vector, states, rewards, value function
+			whfs, whf_freqs, whf_durs, odors, states = \
+				self.update_states(xs, ys, states, odors, iS)
+			rewards = self.update_rewards(whfs, whf_freqs, whf_durs, odors)
+			Q = self.update_Q(Q, states, actions, rewards, iS)
+		
+		# Save normalized Q values at point that trajectory reaches source 
+		# (or end of video)
+		Q_terminal = np.zeros((self._num_states, self._num_actions))
+		for iW in range(self.num_walkers):
+			Q_terminal += Q[path_lengths[iW] - 1, :, :, iW]
+		
+		return xs, ys, Q, Q_terminal, path_lengths
+	
+	def run_optimal_action(self, Q_terminal, seed=0, temp=100):
+		"""
+		Do 1 run using optimal action from Q-function; epsilon-greedy
+		"""
+
+		np.random.seed(seed)
+		self.init_vid()
+		xs, ys, thetas, states, _, ends, path_lengths, odors = self.init_vars()
+		
+		for iS in range(self.num_steps - 1):
+			sys.stdout.write('\r')
+			sys.stdout.write("{:.1f}%".format((100/(self.num_steps - 1)*iS)))
+			sys.stdout.flush()
+
+			# Assign optimal actions; epsilon-greedy; update states
+			actions = self.update_actions_softmax(Q_terminal, states, temp, iS)
+			xs, ys, thetas = self.update_xys(xs, ys, thetas, actions, iS)
+
+			# Mark those that reached the source and how long to do so
+			ends, path_lengths = \
+				self.get_terminal_paths(xs, ys, ends, path_lengths, iS)
+			
+			# Grab odor signal from current frame; cycle video if reached end
+			frm = self.update_vid_frm()
+			
+			# Update odor vector and state
+			whfs, whf_freqs, whf_durs, odors, states = \
 				self.update_states(xs, ys, states, odors, iS)
 		
 		return xs, ys, path_lengths
